c5e267b445ae2168017a865211dde49b07c6dd43
kridi32 <kridi32@outlook.com>
2022-04-17 19:51:38 +0600

    basic authentication done

--- src/components/Pages/Login/Login.js
+++ src/components/Pages/Login/Login.js
@@ -1,20 +1,68 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { Link } from "react-router-dom";
+import auth from "../../../firebase.init";
 const Login = () => {
-    return (
-        <div className='bg-komola h-full w-full'>
-           <div className="container mx-auto flex items-center justify-center py-8 md:py-20">
-                <form action="" className='w-[420px] p-6 bg-white shadow-md'>
-                    <h1 className='text-grey text-3xl font-bold text-center'>Login</h1>
-                    <input type="email" placeholder='Your Email ' className='mt-4  border-0 border-b-2 border-komola p-3 w-full' />
-                    <input type="password" placeholder='Your Password ' className='mt-4  border-0 border-b-2 border-komola p-3 w-full' />
-                    <small className='text-grey'>Forget password? <Link to='/' className='text-komola'>Reset your password</Link></small>
-                    <button className='text-white p-2 bg-komola w-full mt-4 text-2xl '>Login</button>
-                    <small className='text-grey'>New to Gymgainer?<Link to='/register' className='text-komola'> Register here</Link></small>
-                </form>
-           </div>
-        </div>
-    );
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const [
+    signInWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useSignInWithEmailAndPassword(auth);
+  const [loginError, setLoginError] = useState("");
+  const resetPassword = () => {
+      console.log("reset password link has been sent ");
+  }
+  const login = (e) => { 
+      e.preventDefault();
+      signInWithEmailAndPassword(loginEmail, loginPassword)
+  }
+  if(error){
+      setLoginError(error.message)
+  }
+  return (
+    <div className="bg-komola h-full w-full">
+      <div className="container mx-auto flex items-center justify-center py-8 md:py-20">
+        <form action="" className="w-[420px] p-6 bg-white shadow-md" onSubmit={login}>
+          <h1 className="text-grey text-3xl font-bold text-center">Login</h1>
+          <input
+            type="email"
+            placeholder="Your Email "
+            className="mt-4  border-0 border-b-2 border-komola p-3 w-full bg-orange-500 bg-opacity-10"
+            onChange={(e) => setLoginEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Your Password "
+            className="mt-4  border-0 border-b-2 border-komola p-3 w-full bg-orange-500 bg-opacity-10"
+            onChange={(e) => setLoginPassword(e.target.value)}
+          />
+          <small className="text-grey">
+            Forget password?{" "}
+            <span className="text-komola" onClick={resetPassword}>
+              Reset your password
+            </span>
+          </small>
+          <button
+            className="text-white p-2 bg-komola w-full mt-4 text-2xl "
+            type="submit"
+          >
+            Login
+          </button>
+          <small className="text-red-700 block">{loginError}</small>
+          <small className="text-grey">
+            New to Gymgainer?
+            <Link to="/register" className="text-komola">
+              {" "}
+              Register here
+            </Link>
+          </small>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/components/Pages/Register/Register.js
+++ src/components/Pages/Register/Register.js
@@ -1,36 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
+import {useCreateUserWithEmailAndPassword} from "react-firebase-hooks/auth"
+import auth from "../../../firebase.init"
+import { useNavigate } from "react-router";
 const Register = () => {
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth);
+    // declaring variablaes for auth
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [regError, setRegError] = useState("");
+    const navigate = useNavigate();
+    // registration function 
+    const register = (e) => {
+        e.preventDefault();
+        console.log(name, email, password, confirmPassword);
+        if (password === confirmPassword) {
+            createUserWithEmailAndPassword(email, password);
+        } else {
+            setRegError("Passwords did not matched")
+        }
+    }
+    if(error){
+        let err = error.message;
+        setRegError(err);
+    }
+    if(user){
+        navigate("/login");
+    }
   return (
     <div className="bg-komola h-full w-full">
       <div className="container mx-auto flex items-center justify-center py-8 md:py-20">
-        <form action="" className="w-[420px] p-6 bg-white shadow-md">
+        <form action="" className="w-[420px] p-6 bg-white shadow-md" onSubmit={register}>
           <h1 className="text-grey text-3xl font-bold text-center">Register</h1>
           <input
             type="text"
             placeholder="Your Name"
-            className="mt-4  border-0 border-b-2 border-komola p-3 w-full"
+            className="mt-4  border-0 border-b-2 border-komola bg-orange-500 bg-opacity-10 p-3 w-full"
+            onChange={(e) => setName(e.target.value)}
           />
           <input
             type="email"
             placeholder="Your Email "
-            className="mt-4  border-0 border-b-2 border-komola p-3 w-full"
+            className="mt-4  border-0 border-b-2 border-komola p-3 w-full  bg-orange-500 bg-opacity-10" 
+            onChange={(e) => setEmail(e.target.value)}
           />
           <input
             type="password"
             placeholder="Your Password "
-            className="mt-4  border-0 border-b-2 border-komola p-3 w-full"
+            className="mt-4  border-0 border-b-2 border-komola p-3 w-full  bg-orange-500 bg-opacity-10"
+            onChange={(e) => setPassword(e.target.value)}
           />
           <input
             type="password"
             placeholder="Confirm Password "
-            className="mt-4  border-0 border-b-2 border-komola p-3 w-full"
+            className="mt-4  border-0 border-b-2 border-komola p-3 w-full  bg-orange-500 bg-opacity-10"
+            onChange={(e) => setConfirmPassword(e.target.value)}
           />
 
-          <button className="text-white p-2 bg-komola w-full mt-4 text-2xl ">
+          <button className="text-white p-2 bg-komola w-full mt-4 text-2xl " type="submit">
             Register
           </button>
+          <p className="text-red-700 block"> {regError}</p>
           <small className="text-grey">
             Already registered?
             <Link to="/login" className="text-komola">
